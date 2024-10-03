16fc6bd25a3ff4248a745707f4be9b1b9e6cbb07
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-10-03 16:53:20 +0530

    day2-problem2

--- Recursion-Day2/findTheWinner.js
+++ Recursion-Day2/findTheWinner.js
@@ -1,3 +1,5 @@
+// Approach one
+
 var findTheWinner = function (n, k) {
   let arr = Array.from({ length: n }, (_, i) => i + 1);
 
@@ -13,4 +15,27 @@ var findTheWinner = function (n, k) {
   }
 
   return helper(arr, 0);
+};
+
+// Approach two
+
+var findTheWinner = function (n, k) {
+  function jspon(n) {
+    if (n === 0) return 1;
+
+    return (jspon(n - 1) + k) % n;
+  }
+  return jspon(n) + 1;
+};
+
+//  Approach three
+
+var findTheWinner = function (n, k) {
+  let survivor = 0;
+
+  for (let i = 2; i <= n; i++) {
+    survivor = (survivor + k) % i;
+  }
+
+  return survivor + 1;
 };
